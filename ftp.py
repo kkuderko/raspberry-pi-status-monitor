5d44d0d646756956b16ac6df96ca7d04e9f5154a
kkuderko <kkuderko@users.noreply.github.com>
2021-03-11 23:43:13 +0000

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -1,16 +1,17 @@
 import os
 import ftplib
 
-pi_model = os.popen('cat /proc/device-tree/model').read()
 timestamp = os.popen('date +"%F %T"').read()
 pi_boot = os.popen('uptime -s').read()
 pi_uptime = os.popen('uptime -p').read()
-file_name = '/home/pi/python/pi_uptime.txt'
+pi_temp = os.popen('cat /sys/class/thermal/thermal_zone0/temp').read()
+pi_model = os.popen('cat /proc/device-tree/model').read()
 
+file_name = '/home/pi/python/pi_uptime.txt'
 f = open(file_name, 'w')
-f.write(timestamp + pi_boot + pi_uptime + pi_model)
+f.write(timestamp + pi_boot + pi_uptime + pi_temp + pi_model)
 f.close()
 
-ftp = ftplib.FTP('yourhost.com', 'ftpuser@yourhost.com', 'password')
+ftp = ftplib.FTP('yourdomain.com', 'ftpuser@yourdomain.com', 'ftppassword')
 ftp.storbinary('STOR pi_uptime.txt', open(file_name, 'rb'))
 ftp.quit()
